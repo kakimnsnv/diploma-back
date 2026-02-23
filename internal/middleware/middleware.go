38e1b334bfff47994fbfe28e9185c5926cf37289
Kakimbek Nyssanov <kakimbekn@gmail.com>
2026-02-23 09:35:38 +0000

    fix

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -58,6 +58,7 @@ func CORSMiddleware() gin.HandlerFunc {
 			"http://localhost:3000",
 			"http://localhost:3001",
 			"http://127.0.0.1:3000",
+			"http://127.0.0.1:8002",
 			"https://mri-ai.nsnv.kz",
 		}
 
